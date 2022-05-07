4ef982b741c60ab25cf968b637c6940d31c7d5b6
Fabricio Monteiro <fabriciocostamonteiro@hotmail.com>
2022-05-07 16:03:06 -0300

    conexão com mysql

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
 
 function App(){
+  const [values, setValues] = useState();
+
+  function handleChangeValues(e){
+    setValues((prevValue) =>({
+      ...prevValue,
+      [e.target.name]: e.target.value
+
+    }))
+  }
+
+  function handleClickButton(){
+    console.log(values)
+  }
+
   return(
     <div className="app-container">
       <div className="register-container">
-        <h1>Scrim Shop</h1>
+        <h1 className="register-title">Scrim Shop</h1>
         <input
         type="text"
         name="name"
         placeholder="Nome"
         className="register-input"
+        onChange={handleChangeValues}
         />
 
         <input
@@ -18,6 +34,7 @@ function App(){
         name="cost"
         placeholder="Custo"
         className="register-input"
+        onChange={handleChangeValues}
         />
 
         <input
@@ -25,9 +42,10 @@ function App(){
         name="category"
         placeholder="Categoria"
         className="register-input"
+        onChange={handleChangeValues}
         />
         
-        <button>Cadastrar</button>
+        <button className="register-button" onClick={() => handleClickButton()}>Cadastrar</button>
       </div>
     </div>
   )
